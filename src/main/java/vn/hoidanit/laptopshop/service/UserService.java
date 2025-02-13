3b35eac5c6f5bf1ad72524cd5f7bb721935c6a09
Quang Hieu <quanghieu2520230300@gmail.com>
2025-02-13 10:29:39 +0700

    lesson 125

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -31,7 +31,7 @@ public class UserService {
     }
 
     public List<User> getAllUsersByEmail(String email) {
-        return this.userRepository.findByEmail(email);
+        return this.userRepository.findOneByEmail(email);
     }
 
     public User handleSaveUser(User hoidanit) {
@@ -61,5 +61,9 @@ public class UserService {
 
     public boolean checkExistEmail(String email) {
         return this.userRepository.existsByEmail(email);
+    }
+
+    public User getUserByEmail(String email) {
+        return this.userRepository.findByEmail(email);
     }
 }
