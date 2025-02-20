51e777e4044371425e24f1fee0bff9e14b4f3184
Quang Hieu <quanghieu2520230300@gmail.com>
2025-02-20 14:28:11 +0700

    lesson 155

--- src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
@@ -6,9 +6,13 @@ import vn.hoidanit.laptopshop.domain.Product;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     Page<Product> findAll(Pageable pageable);
+
+    Page<Product> findAll(Specification<Product> spec, Pageable pageable);
 }
